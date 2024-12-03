2cbdb2c32ab683f5820fbe991b59ee80543e5644
Stanton Melvin <67967287+RevStanton@users.noreply.github.com>
2024-12-03 18:43:12 +0000

    Allow indexing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pythagorean-quiz/',
+  server: {
+    cors: true,
+  },
+  publicDir: 'public',
   plugins: [react()],
-  base: "/pythagorean-quiz/", // Set this to match your repository name
+
+  
 });
+
